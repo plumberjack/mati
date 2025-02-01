03b3193b8e1c104f9810552e215d20e23a5957cf
NorweskiDrwal <devutnia@gmail.com>
2025-02-01 17:50:10 +0100

    updated uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -14,13 +14,12 @@ fi
 # Remove .mati from PATH in shell configuration files
 remove_from_path() {
   local FILE="$1"
-  local DIR="$2"
   if [ -f "$FILE" ]; then
-    if grep -q "export PATH=\"\$PATH:$DIR\"" "$FILE"; then
-      echo "Removing $DIR from PATH in $FILE..."
-      sed -i.bak "/export PATH=\"\$PATH:$DIR\"/d" "$FILE"
+    if grep -q "export PATH=\"\$PATH:$MATI_DIR\"" "$FILE"; then
+      echo "Removing $MATI_DIR from PATH in $FILE..."
+      sed -i.bak "/export PATH=\"\$PATH:$MATI_DIR\"/d" "$FILE"
     else
-      echo "$DIR not found in PATH in $FILE. Skipping."
+      echo "$MATI_DIR not found in PATH in $FILE. Skipping."
     fi
   else
     echo "$FILE does not exist. Skipping."
@@ -28,27 +27,8 @@ remove_from_path() {
 }
 
 # Remove from .bashrc and .zshrc
-remove_from_path "$HOME/.bashrc" "$MATI_DIR"
-remove_from_path "$HOME/.zshrc" "$MATI_DIR"
-
-# Check if bun is installed and ask if the user wants to remove it
-if command -v bun &> /dev/null; then
-  read -p "Do you want to remove bun? (default: no) [yes/no]: " REMOVE_BUN
-  REMOVE_BUN=${REMOVE_BUN:-no} # Default to "no" if no input is provided
-
-  if [[ "$REMOVE_BUN" == "yes" || "$REMOVE_BUN" == "y" ]]; then
-    echo "Removing bun..."
-    rm -rf "$HOME/.bun"
-    # Remove bun from PATH in shell configuration files
-    remove_from_path "$HOME/.bashrc" "$HOME/.bun"
-    remove_from_path "$HOME/.zshrc" "$HOME/.bun"
-    echo "bun has been removed."
-  else
-    echo "Skipping bun removal."
-  fi
-else
-  echo "bun is not installed. Skipping removal."
-fi
+remove_from_path "$HOME/.bashrc"
+remove_from_path "$HOME/.zshrc"
 
 # Notify the user
 echo "Uninstallation complete! 'mati' has been removed!"
